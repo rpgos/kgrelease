dc0de703d8e0dc623e8ff308df4e295608b16f52
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-05-03 21:41:38 +0200

    Catch errors fetching albums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { fetchAlbums } from "@/utils/api";
+import { notFound } from "next/navigation";
 
 
 export default async function Home() {
-  const albums = await fetchAlbums()
-  
-  const lastReleaseDate = albums.items[0].release_date
-
-  return (
-    <div>Last release on {lastReleaseDate}</div>
-  );
+  try {
+    const albums = await fetchAlbums()
+    
+    const lastReleaseDate = albums.items[0].release_date
+    
+    return (
+      <div>Last release on {lastReleaseDate}</div>
+    );
+  } catch(error) {
+    return notFound()
+  }
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 async function getToken(): Promise<string> {
   const client_id = process.env.SPOTIFY_CLIENT_ID; 
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-  
+
   // token valid for one hour
   const response = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
@@ -20,11 +20,16 @@ async function getToken(): Promise<string> {
 }
 
 export async function fetchAlbums() {
-  const accessToken = await getToken()
-  const response = await fetch("https://api.spotify.com/v1/artists/6XYvaoDGE0VmRt83Jss9Sn/albums?limit=5", {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + accessToken },
-  });
-
-  return await response.json();
+  try {
+    const accessToken = await getToken()
+    const response = await fetch("https://api.spotify.com/v1/artists/6XYvaoDGE0VmRt83Jss9Sn/albums?limit=5", {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + accessToken },
+    });
+  
+    return await response.json();
+  } catch(error) {
+    console.error('Error fetching albums:', error);
+    throw new Error('Could not get albums');
+  }
 }
