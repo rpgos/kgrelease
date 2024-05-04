64f3c55ae8b038468f06c90477c52a113606ea2f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-05-04 11:35:55 +0200

    Set cache TTL and add SpotifyAlbum type

---
+++ src/@types/spotifyAlbum.d.ts
@@ -0,0 +1,9 @@
+export interface SpotifyAlbum {
+  name: string
+  release_date: string
+  images: {
+    url: string
+    height: string
+    width: string
+  }[]
+}
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,5 @@
+import { SpotifyAlbum } from "@/@types/spotifyAlbum";
+
 async function getToken(): Promise<string> {
   const client_id = process.env.SPOTIFY_CLIENT_ID; 
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
@@ -12,6 +14,7 @@ async function getToken(): Promise<string> {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
     },
+    next: { revalidate: 86_400 }, // one day
   });
 
   const res = await response.json()
@@ -19,15 +22,18 @@ async function getToken(): Promise<string> {
   return res.access_token;
 }
 
-export async function fetchAlbums() {
+export async function fetchAlbums(): Promise<SpotifyAlbum[]> {
   try {
     const accessToken = await getToken()
     const response = await fetch("https://api.spotify.com/v1/artists/6XYvaoDGE0VmRt83Jss9Sn/albums?limit=5", {
       method: 'GET',
       headers: { 'Authorization': 'Bearer ' + accessToken },
+      next: { revalidate: 86_400 } // one day
     });
   
-    return await response.json();
+    const res = await response.json()
+
+    return res.items
   } catch(error) {
     console.error('Error fetching albums:', error);
     throw new Error('Could not get albums');
