a18157856627d63b542f3a04f5b66f095b203b4f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-05-04 21:54:12 +0200

    Add remote image pattern to nextconfig, show last release's artwork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { fetchAlbums } from "@/utils/api";
 import { notFound } from "next/navigation";
-
+import { SpotifyAlbum } from "@/@types/spotifyAlbum";
+import Image from "next/image";
 
 export default async function Home() {
   try {
     const albums = await fetchAlbums()
-    
-    const lastReleaseDate = albums.items[0].release_date
+    const lastReleaseDate = albums[0].release_date
+    const lastReleaseImg = albums[0].images[0].url
     
     return (
-      <div>Last release on {lastReleaseDate}</div>
+      <div>
+        Last release on {lastReleaseDate}
+        <Image src={lastReleaseImg} alt="album cover" width={parseInt(albums[0].images[0].width)} height={parseInt(albums[0].images[0].height)} />
+      </div>
     );
   } catch(error) {
     return notFound()
