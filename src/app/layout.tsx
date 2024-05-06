db98c641887b6cbc8d7133efeed267358c0c53ca
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-05-06 09:22:42 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
               height={130}
               alt="Nonagon"
             />
+            <Analytics />
           </div>
         </main>
       </body>
