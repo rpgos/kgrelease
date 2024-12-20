26bf054d73102ce115f9f07fb20b286389fc3ac1
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-20 14:56:06 +0000

    Add auth with email provider

---
+++ src/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./db"
+ 
+export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ],
+})
