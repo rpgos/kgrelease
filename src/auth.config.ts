b465017efb38691d1e4f73fd834924b7bc62c218
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-21 18:20:15 +0000

    Separate auth and auth config

---
+++ src/auth.config.ts
@@ -0,0 +1,18 @@
+import type { NextAuthConfig } from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+ 
+export default {
+  providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ],
+} satisfies NextAuthConfig
--- src/auth.ts
+++ src/auth.ts
@@ -1,21 +1,10 @@
 import NextAuth from "next-auth"
-import Nodemailer from "next-auth/providers/nodemailer"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./db"
+import authConfig from "./auth.config"
  
 export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    Nodemailer({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    })
-  ],
+  session: { strategy: 'jwt' },
+  ...authConfig
 })
