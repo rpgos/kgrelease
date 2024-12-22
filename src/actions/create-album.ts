b32b3e98e062f47ee6fe32988fae927d916ee401
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-22 10:58:50 +0000

    Add create album server action

---
+++ src/actions/create-album.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import { auth } from "@/auth";
+import { prisma } from "@/db";
+
+interface CreateAlbumFormState {
+  errors: {
+    link?: string[],
+    _form?: string[]
+  }
+}
+
+export async function createAlbum(formData: FormData): Promise<CreateAlbumFormState> {
+  const session = await auth()
+
+  if(!session || !session.user) {
+    return { errors: { _form: ['You must be signed in to do this.'] } }
+  }
+
+  const link = formData.get('link') as string;
+
+  try {
+    const album = await prisma.album.create({
+      data: {
+        link,
+      }
+    });
+
+    console.log("Album created: ", album)
+
+  } catch (error) {
+    if(error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message]
+        }
+      }
+    }
+  }
+
+  return { errors: {} }
+}
