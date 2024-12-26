f9df85e06bb591b1c7714e26703bfc85b4c79e42
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-26 14:22:22 +0000

    Refactor album list and items

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,14 @@
 import AlbumPopover from "@/components/album-popover";
 import { davidaFont } from "@/app/fonts";
-import AlbumListItem from "@/components/album-list-item";
 import AlbumList from "@/components/album-list";
-import { getAlbums } from "@/actions/get-albums";
 
 export default async function DashboardPage() {
-  const albums = await getAlbums()
 
   return (
     <div className="text-white h-full w-[755px] relative z-20 overflow-scroll gap-4 flex flex-col items-center">
       <h1 className={`${davidaFont.className} text-4xl p-6`}>Backstage</h1>
       <AlbumPopover />
-      <AlbumList>
-        {
-          albums.map((album) => (
-            <AlbumListItem key={album.id} id={album.id} title={album.link} />
-          ))
-        }
-      </AlbumList>
+      <AlbumList />
     </div>
   )
 }
--- src/components/album-list.tsx
+++ src/components/album-list.tsx
@@ -1,12 +1,42 @@
-interface AlbumListProps {
-  children: React.ReactNode
-}
+'use client'
 
-export default function AlbumList({ children }: AlbumListProps) {
+import { getAlbums } from "@/actions/get-albums";
+import { useEffect, useState } from "react";
+import { Album } from "@prisma/client";
+import AlbumListItem from "@/components/album-list-item";
+import { deleteAlbum } from "@/actions/delete-album";
+import { toast } from "react-toastify";
+
+export default function AlbumList() {
+  const [albums, setAlbums] = useState<Album[]>([]);
+  
+  useEffect(() => {
+    async function loadAlbums() {
+      const fetchedAlbums = await getAlbums();
+      setAlbums(fetchedAlbums);
+    }
+    loadAlbums();
+  }, []);
+
+  const handleDeleteAlbum = async (id: number) => {
+    const res = await deleteAlbum({ id })
+    
+    if(!res.errors._form) {
+      toast.success("Album deleted")
+      setAlbums((prevAlbums) => prevAlbums.filter(album => album.id !== id));
+    } else {
+      toast.error(res.errors._form.join(", "))
+    }
+  };
+
   return (
     <section className="flex flex-col gap-4 w-full p-4 items-center">
       <h2 className="text-2xl">Album List</h2>
-      {children}
+      {
+        albums.map((album) => (
+          <AlbumListItem key={album.id} id={album.id} title={album.link} handleDelete={handleDeleteAlbum} />
+        ))
+      }
     </section>
   )
 }
