0220439c46d352000f84ac99bf0c3ff9cdcd3f6e
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-30 09:24:42 +0000

    Manually run migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+async function initializeDatabase() {
+  try {
+    await prisma.$executeRawUnsafe('npx prisma migrate deploy');
+    console.log('Migrations applied.');
+  } catch (error) {
+    console.error('Failed to apply migrations:', error);
+  }
+}
+
+if(process.env.NODE_ENV === 'production') {
+  initializeDatabase();
+}
