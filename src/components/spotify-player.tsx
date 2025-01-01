9459c9bf27b1f706160e8aedeb52a2ba48b5b092
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-01-01 09:14:55 +0000

    Prepare spotify player to use albums from db

--- src/components/spotify-player.tsx
+++ src/components/spotify-player.tsx
@@ -1,13 +1,34 @@
 'use client'
 
+import { getAlbums } from '@/actions/get-albums'
 import { getRandomAlbum } from '@/utils/getRandomAlbum'
-import React, { useState } from 'react'
+import { Album } from '@prisma/client'
+import React, { useEffect, useState } from 'react'
 
 export default function SpotifyPlayer() {
   const [album, setAlbum] = useState('5LdJz37QiPZ1kFpIva1twi')
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [albums, setAlbums] = useState<Album[]>([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  // useEffect(() => {
+  //   const fetchAlbums = async () => {
+  //     const albums = await getAlbums()
+  //     setAlbums(albums)
+  //     setLoading(false)
+  //   }
+
+  //   fetchAlbums()
+  // }, [])
 
   const getAnotherAlbum = (): void => {
+    // const nextIndex = currentIndex + 1
+    // if(nextIndex < albums.length) {
+    //   setCurrentIndex(nextIndex)
+    //   return
+    // }
+
+    // setCurrentIndex(0)
     let nextAlbum = getRandomAlbum()
     while(nextAlbum === album) {
       nextAlbum = getRandomAlbum()
@@ -19,27 +40,27 @@ export default function SpotifyPlayer() {
   return (
     <>
       {
-        loading &&
+        loading ?
         <div className="justify-center items-center text-white animate-pulse flex h-full">
           <h1 className="text-amber-400">Loading...</h1>
         </div>
-      }
-      <iframe
-        src={`https://open.spotify.com/embed/album/${album}`}
-        width="326"
-        height="432"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-        onLoad={() => setLoading(false)}
-      />
-      {
-        !loading &&
-        <button
-          className="text-amber-400 mt-2 underline-offset-4 underline"
-          onClick={getAnotherAlbum}
-        >
-          Another album
-        </button>
+        :
+        <>
+          <iframe
+            src={`https://open.spotify.com/embed/album/${album}`}
+            width="326"
+            height="432"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          />
+          <button
+            className="text-amber-400 mt-2 underline-offset-4 underline"
+            onClick={getAnotherAlbum}
+          >
+            Another album
+          </button>
+        </>
+
       }
     </>
   )
