6f1b5cc724e632fb1748dcf7ff639be6952db0fc
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-01-03 10:32:15 +0000

    Refactor getToken call

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "@auth/core/jwt"
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true })
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === 'production'
+  })
 
   if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
     const newUrl = new URL("/", req.nextUrl.origin)
