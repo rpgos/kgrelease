6d7e64fdff4ea126411088678097bc20dc393cf9
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-01-03 10:21:29 +0000

    Add secureCookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,9 @@ import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "@auth/core/jwt"
 
 export async function middleware(req: NextRequest) {
-  const session = await getToken({ req, secret: process.env.AUTH_SECRET })
-  // @ts-ignore
-  const user = req.auth?.user
-  console.log({ session, user })
-  if (req.nextUrl.pathname.startsWith('/dashboard') && (!session || user)) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true })
+
+  if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
     const newUrl = new URL("/", req.nextUrl.origin)
     return NextResponse.rewrite(newUrl)
   }
